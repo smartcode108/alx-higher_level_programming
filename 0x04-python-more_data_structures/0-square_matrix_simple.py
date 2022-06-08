a14f0edf74273aab0fdf0ec763c034151afecae1
smartcode108 <smartbrainnetwork@gmail.com>
2022-06-09 00:23:56 +0100

    task3

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+
+    for num in matrix:
+        new_matrix.append(list(map(lambda x: x**2, num)))
+
+    return new_matrix
